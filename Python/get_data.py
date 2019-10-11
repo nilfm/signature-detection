96839c83d39a3c7c276fda787393c51ccdc9ace1
nilfm <nil.fons@gmail.com>
2019-10-11 23:51:04 +0200

    Downloads images from server

--- Python/get_data.py
+++ Python/get_data.py
@@ -1,12 +1,18 @@
 import ftputil
+import os
+
+DATA_PATH = "../../Data"
 
 password = 																																									"andy99.Huguet"
+
+if not os.path.isdir(DATA_PATH):
+	os.mkdir(DATA_PATH)
 
 with ftputil.FTPHost("ftp.ipage.com", "andreuhuguet78654", password) as ftp_host:
 	ftp_host.chdir("hackupc/imgs")
 	names = ftp_host.listdir(ftp_host.curdir)
 	for name in names:
 		print(name)
-		ftp_host.download_if_newer(name, f"../../Data/{name}")
+		ftp_host.download_if_newer(name, os.path.join(DATA_PATH, name))
 		
 	
